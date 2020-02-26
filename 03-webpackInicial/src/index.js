146e8c065bcc8f704c87b82aaedaf6513a4f061c
Adrian <adrianbravo145@gmail.com>
2020-02-26 20:57:27 -0300

    webpack

---
+++ 03-webpackInicial/src/index.js
@@ -0,0 +1,9 @@
+import {saludar} from './js/componentes'
+
+const nombre = 'Adrian'
+
+saludar(nombre);
+
+// instalacion de webpack npm install webpack webpack-cli --save-dev
+// nom run build,ejecuta webpack pordefecto
+
