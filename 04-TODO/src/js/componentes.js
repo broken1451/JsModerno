1eb424e286efbabe4b73459d264a1ad802fdce41
Adrian <adrianbravo145@gmail.com>
2020-03-02 23:23:34 -0300

    agregar nueva tarea con addEventListener

--- 04-TODO/src/js/componentes.js
+++ 04-TODO/src/js/componentes.js
@@ -1,7 +1,9 @@
+import { TODO } from "../classes/index";
+import {todoList} from '../index'
 
 // Referencias en el html
 const divUlTodoList = document.querySelector('.todo-list');
-
+const txtInput = document.querySelector('.new-todo')
 
  export const crearTodoHtml = (todo)=> {
 
@@ -18,12 +20,23 @@ const divUlTodoList = document.querySelector('.todo-list');
     const divUl = document.createElement('div');
     divUl.innerHTML = htmlTodo;
 
-    divUlTodoList.append(divUl.firstElementChild);
+    divUlTodoList.append(divUl.firstElementChild); // regresa el li dentro del div
     return divUl.firstElementChild;
  }
 
 
-
+ // Eventos key code es la tecla q presiono el usuario
+ txtInput.addEventListener('keyup', (event) => {
+    // console.log(event);
+    if (event.keyCode == 13 && txtInput.value.length > 0) {
+       console.log(txtInput.value);
+       const nuevoTodo = new TODO(txtInput.value);
+       todoList.nuevaTarea(nuevoTodo);
+       crearTodoHtml(nuevoTodo);// crea el html
+       txtInput.value = '';
+       console.log(todoList);
+     } 
+ });
 
 
 
