6bda0c4314388f8d411a81dc17dceb258aa19fb9
Adrian <adrianbravo145@gmail.com>
2020-03-07 00:58:13 -0300

    LocalStorage

--- 04-TODO/src/index.js
+++ 04-TODO/src/index.js
@@ -10,14 +10,28 @@ export const todoList = new TodoList()
 
 
 console.log(todoList);
+console.log(todoList.todos);
+todoList.todos.forEach(todo => {
+    console.log([todo]);
+    crearTodoHtml(todo);
+});
+
+// otra amnera 
+// todoList.todos.forEach(crearTodoHtml);
+
+// for (const todo of todoList.todos) {
+//     console.log('todo for of ',todo);
+//     crearTodoHtml(todo)
+// }
+
 
 // crearTodoHtml(tarea);
 
 
 // localstorage es que la informacion almacenada en localstorage no posee tiempo de expiracion se puede cerrar o reiniciar el navegador y la data estara alli 
 // session storage se borra todo cuando se cierra por completo el navgador web 
-localStorage.setItem('key','ABC');
-sessionStorage.setItem('key','ABC');
+// localStorage.setItem('key','ABC');
+// sessionStorage.setItem('key','ABC');
 // setTimeout(() => {
 //     localStorage.removeItem('key');
-// }, 1500);
+// }, 1500);   
